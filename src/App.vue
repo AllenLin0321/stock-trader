989275cd3ad4517b34cfe3c244e970c1d6fdda98
Allen <glux7381@gmail.com>
2019-05-15 17:14:18 +0800

    Revert "Finish vue-resource function"
    This reverts commit 8e6d7ce66f598ec773175d781e3f824a34e62406.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
     <b-container>
         <Header></Header>
         <b-col md="12">
-            <transition name="slide" mode="out-in">
-                <router-view></router-view>
-            </transition>
+            <router-view></router-view>
         </b-col>
     </b-container>
 
@@ -26,38 +24,5 @@
 <style lang="scss">
     body {
         padding: 30px;
-    }
-
-    .slide {
-        &-enter-active {
-            animation: slide-in 200ms ease-out forwards;
-        }
-
-        &-leave-active {
-            animation: slide-out 200ms ease-out forwards;
-        }
-    }
-
-    @keyframes slide-in {
-        from {
-            transform: translateY(-30px);
-            opacity: 0;
-        }
-        to {
-            transform: translateY(0);
-            opacity: 1;
-        }
-    }
-
-    @keyframes slide-out {
-        from {
-            transform: translateY(0);
-            opacity: 1;
-        }
-
-        to {
-            transform: translateY(-30px);
-            opacity: 0;
-        }
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,15 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store/store';
 import BootstrapVue from 'bootstrap-vue';
-import VueResource from 'vue-resource';
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue);
-Vue.use(VueResource);
-Vue.http.options.root = 'https://vuejs-stock-trader-97cf2.firebaseio.com/'
-
 Vue.config.productionTip = false;
 
 Vue.filter('currency', value => {
